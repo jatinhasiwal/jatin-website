d5b4994eb6bef824f010fa3429de2d15e37527bb
Jatin Hasiwal <140293091+jatinhasiwal@users.noreply.github.com>
2026-02-18 22:28:30 +0530

    Refactor Portfolio heading styles and layout

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -49,8 +49,15 @@ export default function Portfolio() {
       <div className="relative max-w-7xl mx-auto px-6 lg:px-12">
         {/* Heading */}
         <div className="text-center max-w-3xl mx-auto mb-16">
-          <div className="inline-block bg-[#E8602E]/10 border border-[#E8602E]/30 text-[#E8602E] px-4 py-2 rounded-full text-sm font-semibold mb-6">
+          <div className="relative w-fit mx-auto font-semibold text-lg text-[#E8602E]  bg-[#E8602E21] inline-block border border-[#E8602E] px-2 font-machina pt-[.06rem] mb-4">
             My Work
+            <span className="absolute aspect-square h-1  bg-white z-10 top-0 left-0 -translate-x-1/2 -translate-y-1/2"></span>
+              {/* Top Right Dot */}
+              <span className="absolute aspect-square h-1  bg-white z-10 top-0 right-0 translate-x-1/2 -translate-y-1/2"></span>
+              {/* Bottom Right Dot */}
+              <span className="corner-dot-br absolute aspect-square h-1 bg-white z-10 bottom-0 right-0 translate-x-1/2 translate-y-1/2"></span>
+              {/* Bottom Left Dot */}
+              <span className="absolute aspect-square h-1 bg-white bottom-0 left-0 -translate-x-1/2 translate-y-1/2"></span>
           </div>
 
           <h2 className="text-4xl lg:text-5xl font-bold text-white mb-6">
